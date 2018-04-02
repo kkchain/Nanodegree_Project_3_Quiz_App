6c251ff70b75e35cf28375bac3e35e43349ebd50
KK.Chain <33008247+kkchain@users.noreply.github.com>
2018-04-01 21:28:28 -0500

    Initialize views in onCreate added new picture added background color move hard coding into resources file

--- app/src/main/java/com/example/android/project3quiz/MainActivity.java
+++ app/src/main/java/com/example/android/project3quiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.project3quiz;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -10,62 +11,82 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Initialized
     int totalScore = 0;
+    String userName;
+    String favoriteFruits;
+    String displayResult;
+    EditText nameInput;
+    EditText fruitInput;
+    RadioButton question_1Button;
+    RadioButton question_2Button;
+    RadioButton question_4Button;
+    RadioButton question_5Button;
+    RadioButton question_6Button;
+    CheckBox indonesia_Checkbox;
+    CheckBox malaysia_Checkbox;
+    CheckBox thailand_Checkbox;
+    CheckBox native_Checkbox;
+    CheckBox spikes_Checkbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hide Android EditTExt Keyboard Each Time the app started
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
     public void submitAnswers(View view) {
         // get user name
-        EditText nameInput = (findViewById(R.id.enter_name));
+        nameInput = (findViewById(R.id.enter_name));
         // assign String to nameInput
-        String userName = nameInput.getText().toString();
+        userName = nameInput.getText().toString();
 
         // get RadioButton ID from Question 1
-        RadioButton question_1Button = findViewById(R.id.q1_durian);
+        question_1Button = findViewById(R.id.q1_durian);
         // call method to add 1 point if Question 1 answered correctly
         calculateRadioButtonScores(question_1Button.isChecked());
 
         // get RadioButton ID from Question 2
-        RadioButton question_2Button = findViewById(R.id.q2_true);
+        question_2Button = findViewById(R.id.q2_true);
         // call method to add 1 point if Question 2 answered correctly
         calculateRadioButtonScores(question_2Button.isChecked());
 
         // get CheckBox ID from Question 3
-        CheckBox indonesia_Checkbox = findViewById(R.id.q3_indonesia);
-        CheckBox malaysia_Checkbox = findViewById(R.id.q3_malaysia);
-        CheckBox thailand_Checkbox = findViewById(R.id.q3_thailand);
+        indonesia_Checkbox = findViewById(R.id.q3_indonesia);
+        malaysia_Checkbox = findViewById(R.id.q3_malaysia);
+        thailand_Checkbox = findViewById(R.id.q3_thailand);
+
         // get CheckBox ID from Question 8
-        CheckBox native_Checkbox = findViewById(R.id.q8_native);
-        CheckBox spikes_Checkbox = findViewById(R.id.q8_spikes);
+        native_Checkbox = findViewById(R.id.q8_native);
+        spikes_Checkbox = findViewById(R.id.q8_spikes);
         // call method to add 1 point if Question 3 and 8 answered correctly
         calculateCheckBoxScores(indonesia_Checkbox.isChecked(), malaysia_Checkbox.isChecked(), thailand_Checkbox.isChecked(), native_Checkbox.isChecked(), spikes_Checkbox.isChecked());
 
         // get RadioButton ID from Question 4
-        RadioButton question_4Button = findViewById(R.id.q4_jackfruit);
+        question_4Button = findViewById(R.id.q4_jackfruit);
         // call method to add 1 point if Question 4 answered correctly
         calculateRadioButtonScores(question_4Button.isChecked());
 
         // get RadioButton ID from Question 5
-        RadioButton question_5Button = findViewById(R.id.q5_artocarpus);
+        question_5Button = findViewById(R.id.q5_artocarpus);
         // call method to add 1 point if Question 4 answered correctly
         calculateRadioButtonScores(question_5Button.isChecked());
 
         // get RadioButton ID from Question 6
-        RadioButton question_6Button = findViewById(R.id.q6_mangosteen);
+        question_6Button = findViewById(R.id.q6_mangosteen);
         // call method to add 1 point if Question 6 answered correctly
         calculateRadioButtonScores(question_6Button.isChecked());
 
         // Question 7 get participant favorite fruits
-        EditText fruitInput = (findViewById(R.id.enter_fruits));
+        fruitInput = (findViewById(R.id.enter_fruits));
         // assign String to nameInput
-        String favoriteFruits = fruitInput.getText().toString();
+        favoriteFruits = fruitInput.getText().toString();
 
         // toast
-        String displayResult = userName + ",You Scored " + totalScore + " out of 10" + "\n You liked " + favoriteFruits;
+        displayResult = userName + "\nYou Scored " + totalScore + " out of 10" + "\n You liked " + favoriteFruits;
         Toast.makeText(this, displayResult, Toast.LENGTH_LONG).show();
     }
 
